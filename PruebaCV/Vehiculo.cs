9e0a374315c71e09e04ea5a7854947a8edbd4320
Mrollun-29 <mroldanluna.dev@gmail.com>
2025-02-19 08:45:46 +0100

    Estructura Organizativa del proyecto: Clases Instanciables y Estáticas

---
+++ PruebaCV/Vehiculo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaCV
+{
+
+    /// <summary>
+    /// Clase instanciable - Se pueden generar objetos que se alojarán en memoria
+    /// 
+    /// </summary>
+    public class Vehiculo
+    {
+    }
+}
